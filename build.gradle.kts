bb7481b4cceab27992e9445983c1f7d1670f2444
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-04 21:34:16 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta07")
+    val compose_version by extra("1.0.0-rc01")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
     }
 }
