85d7b9581fb1610574325f4015ddb30c61e7817b
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-09-15 18:59:59 +0200

    - upgrade interactors gradle

---
+++ movie/movie-interactors/src/main/java/com/laurenttizzone/thousandeyes/movie_interactors/GetMovies.kt
@@ -0,0 +1,49 @@
+package com.laurenttizzone.thousandeyes.movie_interactors
+
+import com.laurenttizzone.thousandeyes.core.DataState
+import com.laurenttizzone.thousandeyes.core.ProgressBarState
+import com.laurenttizzone.thousandeyes.core.UIComponent
+import com.laurenttizzone.thousandeyes.movie_domain.Movie
+import com.laurenttizzone.thousandeyes.movie_domain.network.MovieService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetMovies(
+    private val service: MovieService
+    //TODO(Add caching)
+) {
+    fun execute(): Flow<DataState<List<Movie>>> = flow {
+        try {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Loading))
+            val movies: List<Movie> = try {
+                service.getTrendingMovies()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(
+                    DataState.Response<List<Movie>>(
+                        uiComponent = UIComponent.Dialog(
+                            title = "Error",
+                            description = e.message ?: "Unknown Error"
+                        )
+                    )
+                )
+                listOf()
+            }
+            //TODO(caching)
+            emit(DataState.Data(movies))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(
+                DataState.Response<List<Movie>>(
+                    uiComponent = UIComponent.Dialog(
+                        title = "Error",
+                        description = e.message ?: "Unknown Error"
+                    )
+                )
+            )
+        }
+        finally {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
+        }
+    }
+}
--- movie/movie-interactors/src/main/java/com/laurenttizzone/thousandeyes/movie_interactors/PlaceHolder.kt
+++
@@ -1,4 +0,0 @@
-package com.laurenttizzone.thousandeyes.movie_interactors
-
-class PlaceHolder {
-}
