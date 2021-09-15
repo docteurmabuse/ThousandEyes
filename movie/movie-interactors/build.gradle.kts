d7fb6399ba10463fec9b1296104413f035c74e07
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-09-15 18:26:30 +0200

    - upgrade interactors gradle

--- movie/movie-interactors/build.gradle.kts
+++ movie/movie-interactors/build.gradle.kts
@@ -3,5 +3,10 @@ apply{
 }
 
 dependencies{
+    "implementation"(project(Modules.core))
+    "implementation"(project(Modules.movieDataSource))
+    "implementation"(project(Modules.movieDomain))
+
+    "implementation"(Kotlinx.coroutinesCore)
 
 }
